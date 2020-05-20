97168ef11b0ca2538c1b8505336486eac7f03296
Leszek <leszek.synowiec@gmail.com>
2020-05-20 22:28:58 +0200

    add home view and mainLayout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.scss';
+
+import Home from './components/views/Home/Home.component'
 
 function App() {
   return (
     <div className="App">
-
+      <Home />
     </div>
   );
 }
---
+++ src/components/views/Home/Home.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MainLayout from "../../layout/mainLayout.component";
+
+const Home = () => (
+  <div className="Home">
+    <MainLayout>
+      <h1>HomeView</h1>
+    </MainLayout>
+  </div>
+);
+
+export default Home;
