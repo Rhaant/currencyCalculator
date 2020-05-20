3b91ce9af1d280fb2bfefba7f87e6086300e104e
Leszek <leszek.synowiec@gmail.com>
2020-05-20 23:50:15 +0200

    add splash component

---
+++ src/components/splash/splash.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./splash.styles.scss";
+
+const Splash = () => (
+  <div className="Splash">
+    <div className="Wrapper">
+      <div className="Left-side">
+          <div className="Square">
+            <div className="Background-square-one" />
+            <div className="Background-square-two" />
+            <div className="Data-square">
+                <p>
+                All data are based on European central bank and are constantly updated every day
+                </p>
+            </div>
+          </div>
+      </div>
+      <div className="Right-side"></div>
+    </div>
+  </div>
+);
+
+export default Splash;
--- src/components/views/Home/Home.component.jsx
+++ src/components/views/Home/Home.component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 import MainLayout from "../../layout/mainLayout.component";
+import Splash from '../../splash/splash.component'
+
+import './Home.styles.scss';
 
 const Home = () => (
   <div className="Home">
     <MainLayout>
-      <h1>HomeView</h1>
+      <Splash />
     </MainLayout>
   </div>
 );
